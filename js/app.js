f74ed5e40c07cf46daa1043165657c1f5157be97
shprink <julien.renaux@gmail.com>
2013-09-15 23:22:04 +0200

    adding demo and code Iframes

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
 angular.module('democenter', ['demoFactory', 'ngRoute'])
-		.config(function($routeProvider) {
+		.config(function($routeProvider, $sceDelegateProvider) {
+	$sceDelegateProvider.resourceUrlWhitelist([
+		'examples.julienrenaux.fr', 
+		'/template'
+	]);
 	$routeProvider.
 			when('/demos', {templateUrl: 'template/demo-list.html', controller: DemoListCtrl}).
 			when('/demos/:demoId', {templateUrl: 'template/demo-item.html', controller: DemoItemCtrl}).
@@ -9,15 +13,35 @@ angular.module('democenter', ['demoFactory', 'ngRoute'])
 angular.module('demoFactory', ['ngResource'])
 		.factory('Demo', function($resource) {
 	return $resource('data/:demoId.json', {}, {
-		query: {method: 'GET', params: {demoId: 'demos'}, isArray: true}
+		query: {
+			method: 'GET',
+			params: {
+				demoId: 'demos'
+			},
+			isArray: true,
+			cache: false
+		}
 	});
 });
 
-function DemoListCtrl($scope, Demo) {
+// http://jsfiddle.net/moderndegree/yYEXN/
+//angular.module('demoCache', []).
+//		factory('DemoCache', function($cacheFactory) {
+//	return $cacheFactory('someCache', {
+//		capacity: 3
+//	});
+//});
+
+function DemoListCtrl($scope, $cacheFactory, $log, $http, Demo) {
+	var $httpDefaultCache = $cacheFactory.get('$http');
+	$log.log($httpDefaultCache.removeAll());
+	//$cacheFactory.get('$http').removeAll();
 	$scope.list = Demo.query()
 }
 
-function DemoItemCtrl($scope, Demo, $routeParams) {
+DemoListCtrl.$inject = ['$scope', '$cacheFactory', '$log', '$http', 'Demo'];
+
+function DemoItemCtrl($scope, $routeParams, Demo) {
 	Demo.get({
 		demoId: $routeParams.demoId
 	}, function(demo) {
@@ -25,4 +49,8 @@ function DemoItemCtrl($scope, Demo, $routeParams) {
 	}, function(error) {
 		$scope.item = {name: 'Oups Something wrong happened'};
 	});
+	$("#accordion").collapse();
 }
+
+
+DemoItemCtrl.$inject = ['$scope', '$routeParams', 'Demo'];
