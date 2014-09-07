51a46d13cbf6918eb018b0cb1789cec44c135bff
shprink <julien.renaux@gmail.com>
2014-09-07 11:45:27 +0200

    update

--- js/app.js
+++ js/app.js
@@ -3,46 +3,50 @@ angular.module('democenter', [
     'ngRoute',
     'ngProgress',
     'ui.bootstrap'
-]).run(['$rootScope', '$sce', 'DemoService', 'Demo', '$routeParams',
-    function($rootScope, $sce, DemoService, Demo, $routeParams) {
+]).run(['$rootScope', '$sce','Demo', '$routeParams', '$location', '$window',
+    function($rootScope, $sce, Demo, $routeParams, $location, $window) {
         $rootScope.trustSrc = function(src) {
             return $sce.trustAsResourceUrl(src);
         }
 
-        $rootScope.list = (function(){ return Demo.query(function(results) {
-			var categories = {
-				'all': {
-					'name': 'all',
-					'value': '',
-					'count': results.length,
-					'href': '#demos'
-				}
-			};
-			angular.forEach(results, function(value, key) {
-				if (typeof categories[value.category] == 'undefined') {
-					categories[value.category] = {
-						'name': value.category,
-						'count': 0,
-						'value': value.category,
-						'href': '#demos/' + value.category
-					}
-				}
-				categories[value.category]['count'] += 1;
-			});
-			$rootScope.categories = categories;
-		})})();
+        $rootScope.list = (function() {
+            return Demo.query(function(results) {
+                var categories = {
+                    'all': {
+                        'name': 'all',
+                        'value': '',
+                        'count': results.length,
+                        'href': '#demos'
+                    }
+                };
+                angular.forEach(results, function(value, key) {
+                    if (typeof categories[value.category] == 'undefined') {
+                        categories[value.category] = {
+                            'name': value.category,
+                            'count': 0,
+                            'value': value.category,
+                            'href': '#demos/' + value.category
+                        }
+                    }
+                    categories[value.category]['count'] += 1;
+                });
+                $rootScope.categories = categories;
+            })
+        })();
 
-		$rootScope.$on('$routeChangeSuccess', function(scope, next, current){
-			if (typeof next.params.category !== 'undefined'){
-				$rootScope.currentCategory = next.params.category;
-			} else {
-				$rootScope.currentCategory = '';
-			}
-            DemoService.afterLoading();
-		});
+        $rootScope.$on('$routeChangeSuccess', function(scope, next, current) {
+            if (typeof next.params.category !== 'undefined') {
+                $rootScope.currentCategory = next.params.category;
+            } else {
+                $rootScope.currentCategory = '';
+            }
+            $window.ga('send', 'pageview', { page: $location.path() });
+        });
 
         $rootScope.getFbUri = function(category, uri) {
-            return DemoService.getFbUri(category, uri);
+            return '//www.facebook.com/plugins/like.php?href=' + encodeURIComponent(
+                    'http://demos.julienrenaux.fr/#/demos/' + category + '/' + uri) +
+                    '&amp;width=200&amp;height=30&amp;colorscheme=light&amp;layout=button_count&amp;action=like&amp;show_faces=false&amp;send=false';
         }
     }
 ]);
@@ -89,24 +93,6 @@ angular.module('democenter').config(['$routeProvider',
         otherwise({
             redirectTo: '/demos'
         });
-    }
-]);
-
-angular.module('democenter').service('DemoService', ['ngProgress', '$window', '$location',
-    function(ngProgress, $window, $location) {
-
-        this.getFbUri = function(category, uri) {
-            return '//www.facebook.com/plugins/like.php?href=' + encodeURIComponent(
-                'http://demos.julienrenaux.fr/#/demos/' + category + '/' + uri) +
-                '&amp;width=200&amp;height=30&amp;colorscheme=light&amp;layout=button_count&amp;action=like&amp;show_faces=false&amp;send=false';
-        }
-
-        this.afterLoading = function() {
-            var currentPageId = $location.path();
-            if (typeof $window._gaq !== 'undefined') {
-                $window._gaq.push(['_trackPageview', currentPageId]);
-            }
-        }
     }
 ]);
 
